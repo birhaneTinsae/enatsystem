5be84f9e50221fefdd61f334e2dfaa08622724c3
abelmu <abelmekuria1@gmail.com>
2018-06-26 10:42:46 +0300

    Illuminate\Database\Eloquent\SoftDeletes trait added

--- app/AdditionalCost.php
+++ app/AdditionalCost.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class AdditionalCost extends Model
 {
+     use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
       protected $connection = 'sqlsrv2';
+         public function asset(){
+        return $this->belongsTo('App\Asset');
+    }
 }
