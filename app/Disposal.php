f985498932084ba37e7434e9f848f8c838283b68
abelmu <abelmekuria1@gmail.com>
2018-06-26 10:51:19 +0300

    Illuminate\Database\Eloquent\SoftDeletes trait added

--- app/Disposal.php
+++ app/Disposal.php
@@ -3,9 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Disposal extends Model
 {
+       use SoftDeletes;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
       protected $connection = 'sqlsrv2';
        public function asset(){
         return $this->belongsTo('App\Asset');
