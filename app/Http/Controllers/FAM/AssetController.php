3a3b52dd6b7545e902580baddf7344d37ea52d63
abelmu <abelmekuria1@gmail.com>
2018-06-26 09:25:55 +0300

    Asset maintenance controller created

--- app/Http/Controllers/FAM/AssetController.php
+++ app/Http/Controllers/FAM/AssetController.php
@@ -11,6 +11,8 @@ use App\Branch;
 use App\Disposal;
 use App\Employee;
 use App\Transfer;
+use App\Usefullife;
+use App\Residuallife;
 use App\PPECategory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -45,15 +47,8 @@ class AssetController extends Controller
         //dd( $branches);
         $custudian=new Employee;
         $custudian->setconnection('sqlsrv');
-        $custudian=Employee::all();
-        $count=0;
-        $custudian_name=array();
-        foreach($custudian as $cust){
-            $custudian_name[$count]=DB::table('users')->where('id', $cust->user_id)->value('name');
-            $count++;
-        }
-
-        return view('fixed_asset.asset.new',['branches'=>$branches,'emp_name'=>$custudian_name,'Employee'=>$custudian]);
+        $custudian=Employee::all();             
+        return view('fixed_asset.asset.new',['branches'=>$branches,'Employee'=>$custudian]);
     }
 
     /**
@@ -63,22 +58,17 @@ class AssetController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {        
-        // $valid_data = App::make('validation.presence');
-        // $valid_data->setConnection('sqlsrv2');
+    {                   
           $valid_data=$request->validate([
-            'asset_name'=>'required',
-            //'asset_sub_category'=>'required',
-            'asset_category'=>'required',
-            'custudian'=>'required',
+            'asset_name'=>'required',           
+            'asset_category'=>'required',            
             'cost_center'=>'required',          
-        ]);  
-      //  Validator::getPresenceVerifier()->setConnection('sqlsrv2');
-        //dd($request->all());
+        ]);          
         $asset=new Asset;
         $asset->setconnection('sqlsrv2');
         $asset->asset_name=$request->asset_name;
         $asset->purchase_date=$request->purchased_date;
+        $asset->date_of_acquisition=$request->purchased_date;
         $asset->gross_purchase_amount=$request->gross_purchase_amount;
         $asset->overrided_useful_life=$request->useful_life;
         $asset->overrided_residual_value=$request->residual_value;
@@ -92,13 +82,7 @@ class AssetController extends Controller
         $asset->srv=$request->srv;
         $asset->custudian=$request->custudian;
         $asset->serial_no=$request->serial_no;
-        $asset->book_value=$request->gross_purchase_amount;
-
-        // if(!isset($request->disposed)){
-        //     $asset->disposed=false;
-        // }else{
-        //     $asset->disposed=true;
-        // }
+        $asset->book_value=$request->gross_purchase_amount;  
        
         if($asset->save()){
             $request->session()->flash('status',"Asset ".$request->asset_name." successfully added.");
@@ -122,17 +106,30 @@ class AssetController extends Controller
         $additional_costs->setconnection('sqlsrv2');
         $additional_costs=AdditionalCost::where('asset_id',$asset->id)->get();
         $impairments=Impairment::where('asset_id',$asset->id)->get();
+
+        $useful_life=new Usefullife;
+        $useful_life->setconnection('sqlsrv2');
+        $useful_life=Usefullife::where('asset_id',$asset->id)->get();
+
+        $residual_life=new Residuallife;
+        $residual_life->setconnection('sqlsrv2');
+        $residual_life=Residuallife::where('asset_id',$asset->id)->get();
         $branch=new Branch;
         $branch->setconnection('sqlsrv');
         $branch=Branch::find($asset->branch_id);
         $custudian=Employee::find($asset->custudian);
-
-        $custudian_name=$custudian->full_name;
+        //dd($custudian);
+        if($custudian==""){
+            $custudian_name="";
+        }
+        else{
+            $custudian_name=$custudian->full_name;
+        }
+        
         $disposal=new Disposal;
         $disposal->setconnection('sqlsrv2');
         $disposal=Disposal::where('asset_id',$asset->id)->get();
         $transfer=Transfer::where('asset_id',$asset->id)->get();
-       //dd($transfer);
        $count=0;
        $femp_name=array();
        $temp_name=array();
@@ -147,25 +144,13 @@ class AssetController extends Controller
     $fbranch_name[$count]= DB::table('branches')->where('id', $result->from_cost_center)->value('branch_name');
     $tbranch_name[$count]= DB::table('branches')->where('id', $result->to_cost_center)->value('branch_name');
    $count++;
-      }
-      //dd($from_user_id);  
-//     foreach($from_user_id as $res){      
-//    $femp_name[$count2]= DB::table('users')->where('id',$res)->value('name');     
-//    $count2++;
-//       }
-//        $count3=0;       
-//         foreach($to_user_id as $res2){      
-//    $temp_name[$count3]= DB::table('users')->where('id',$res2)->value('name');     
-//    $count3++;
-//       }
-      
-  
-        $asset_category=PPECategory::find($asset->p_p_e_categorie_id);
-         //dd($branch->branch_name);
+      }            
+        $asset_category=PPECategory::find($asset->p_p_e_categorie_id);        
         return view('fixed_asset.asset.show',['asset'=>$asset,'additional_costs'=>$additional_costs,'impairments'=>$impairments,
         'custudian_name'=>$custudian_name,'branch'=>$branch,'Disposal'=>$disposal,
         'asset_category'=>$asset_category,'transfer'=>$transfer,'from_employee'=>$femp_name,
-        'to_employee'=>$temp_name,'FromBranch'=>$fbranch_name,'ToBranch'=>$tbranch_name]);
+        'to_employee'=>$temp_name,'FromBranch'=>$fbranch_name,'ToBranch'=>$tbranch_name
+        ,'Usefullife'=>$useful_life,'Residuallife'=>$residual_life]);
     }
 
     /**
@@ -174,9 +159,13 @@ class AssetController extends Controller
      * @param  \App\Asset  $asset
      * @return \Illuminate\Http\Response
      */
-    public function edit(Asset $asset)
+    public function edit(Request $request,$id)
     {
-        //
+       $edit=Asset::find($id);  
+       $branch=Branch::find($edit->branch_id);   
+       $Custudian=Employee::find($edit->custudian);      
+      return view('fixed_asset.asset.update',['Asset'=>$edit,'Cost_center'=>$branch
+      ,'Custudian'=>$Custudian]);
     }
 
     /**
@@ -186,9 +175,39 @@ class AssetController extends Controller
      * @param  \App\Asset  $asset
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Asset $asset)
+    public function update(Request $request,$id)
     {
-        //
+       // dd($request->all());
+      $valid_data=$request->validate([
+            'asset_name'=>'required',           
+            'asset_category'=>'required',
+            'asset_sub_category'=>'required',
+            'cost_center'=>'required',          
+        ]);          
+        $update= Asset::find($id);
+        $update->setconnection('sqlsrv2');
+        $update->asset_name=$request->asset_name;
+        $update->purchase_date=$request->purchased_date;
+        $update->date_of_acquisition=$request->purchased_date;
+        $update->gross_purchase_amount=$request->gross_purchase_amount;
+        $update->overrided_useful_life=$request->useful_life;
+        $update->overrided_residual_value=$request->residual_value;
+        $update->tag=$request->tag;
+        $update->asset_item_id=$request->asset_sub_category;
+        $update->branch_id=$request->cost_center;
+        $update->remarks=$request->remarks;
+        $update->p_p_e_categorie_id=$request->asset_category;
+        $update->description=$request->description;
+        $update->gr_number=$request->gr_number;
+        $update->srv=$request->srv;
+        $update->custudian=$request->custudian;
+        $update->serial_no=$request->serial_no;
+        $update->book_value=$request->gross_purchase_amount;  
+       
+        if($update->save()){
+            $request->session()->flash('status',"Asset ".$request->asset_name." successfully updated.");
+            return redirect('/asset');
+        }
     }
 
     /**
